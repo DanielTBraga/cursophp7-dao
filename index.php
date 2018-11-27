d845e070be7013493710f54f627dc5aeb0213749
Daniel <danieltbraga@gmail.com>
2018-11-27 08:48:40 -0200

    inserir e alterar

--- index.php
+++ index.php
@@ -16,15 +16,34 @@ require_once("config.php");
 //echo json_encode($search);
 
 //Carrega um usuário usando o login e a senha
-$usuario = new Usuario();
-$usuario -> login("root", "!@#$");
+//$usuario = new Usuario();
+//$usuario -> login("root", "!@#$");
 
-echo $usuario;
+//echo $usuario;
+
+//$aluno -> setDeslogin("aluno");
+//$aluno -> setDessenha("@lun0");
+/*
+//Criando um novo usuário
+$aluno = new Usuario("aluno", "@lun0");
+
+$aluno -> insert();
+
+echo $aluno;
+*/
 
 /*$sql = new Sql();
 
 $usuarios = $sql -> select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);*/
+
+$usuario = new Usuario();
+
+$usuario -> loadById(8);
+
+$usuario -> update("professor", "!@#$%¨&*");
+
+echo $usuario;
 
  ?>
